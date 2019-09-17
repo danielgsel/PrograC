a295349c884e95d28d675db410b87dc4ac1c4499
Dani <dangon21@ucm.es>
2019-09-17 17:39:07 +0200

    pe

---
+++ EjerciciosTema1/Ej.4/cmake-build-debug/CMakeFiles/Ej_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ej_4.dir/Source.cpp.o"
+  "Ej_4.pdb"
+  "Ej_4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ej_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
